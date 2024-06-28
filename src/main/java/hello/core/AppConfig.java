8d6cf0a9082c3bfd5fd391c3f039c32b9972f2bc
홍혁진 <gurwls123164@naver.com>
2024-06-28 18:07:52 +0900

    Spring bean Check

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -2,29 +2,38 @@ package hello.core;
 
 import hello.core.discount.DiscountPoilcy;
 import hello.core.discount.FixDiscountPoilcy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
 
     //역할
-    public MemberService memberService() {
+    @Bean
+    public MemberService memberService(){
         return new MemberServiceImpl(memberRepository());
     }
+
     //역할에 대한 구현
-    private MemoryMemberRepository memberRepository() {
+    @Bean
+    public MemoryMemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
     //역할
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
     //역할에 대한 구현
+    @Bean
     private static DiscountPoilcy discountPolicy() {
-        return new FixDiscountPoilcy();
+        return new RateDiscountPolicy();
     }
 }
